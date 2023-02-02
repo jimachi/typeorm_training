c315975cbc20c72e0acba40e9dc07cd4a96dbea1
Yuichiro Iijima <jimaichiro@gmail.com>
2023-02-03 02:11:58 +0900

    基本的なDB操作を試す

--- MyProject/src/index.ts
+++ MyProject/src/index.ts
@@ -1,3 +1,49 @@
-import { AppDataSource } from "./data-source"
+import { AppDataSource } from "./data-source";
+import { Photo } from "./entity/Photo";
 
-AppDataSource.initialize().then(async () => {}).catch(error => console.log(error))
+AppDataSource.initialize()
+    .then(async () => {
+        const photo = new Photo();
+        photo.name = "Me and Bears";
+        photo.description = "I am near polar bears";
+        photo.filename = "photo-with-bears.jpg";
+        photo.views = 1;
+        photo.isPublished = true;
+
+        const savedPhotos = await AppDataSource.manager.find(Photo);
+        console.log("All photos from the db: ", savedPhotos);
+
+        const photoRepository = await AppDataSource.getRepository(Photo);
+
+        await photoRepository.save(photo);
+        console.log("Photo has been saved");
+        
+        const allPhotos = await photoRepository.find();
+        console.log(`All photos from the db: ${allPhotos}`);
+
+        const firstPhoto = await photoRepository.findOneBy({
+            id: 1,
+        });
+        console.log(`First photo from the db: ${firstPhoto}`);
+
+        const meAndBearsPhoto = await photoRepository.findOneBy({
+            name: "Me and Bears",
+        });
+        console.log(`Me and Bears photo from the db: ${meAndBearsPhoto}`);
+
+        const allViewedPhotos = await photoRepository.findBy({ views: 1 });
+        console.log(`All viewed photos: ${allViewedPhotos}`);
+
+        const allPublishedPhotos = await photoRepository.findBy({
+            isPublished: true,
+        });
+        console.log(`All published photos: ${allPublishedPhotos}`);
+
+        const [photos, photosCount] = await photoRepository.findAndCount();
+        console.log(`All photos: ${photo}`);
+        console.log(`Photos count: ${photosCount}`);
+
+        const savedPhoto = await AppDataSource.manager.find(Photo);
+        console.log(`All photos from the db: ${savedPhoto}`);
+    })
+    .catch((error) => console.log(error));
