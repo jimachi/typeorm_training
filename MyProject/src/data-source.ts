e8651b835bb9454a9cd742562eabc665a6ef4efb
Yuichiro Iijima <jimaichiro@gmail.com>
2023-02-03 01:28:17 +0900

    feat: add Photo table

--- MyProject/src/data-source.ts
+++ MyProject/src/data-source.ts
@@ -1,6 +1,6 @@
 import "reflect-metadata"
 import { DataSource } from "typeorm"
-import { User } from "./entity/User"
+import { Photo } from "./entity/Photo"
 
 export const AppDataSource = new DataSource({
     type: "postgres",
@@ -11,7 +11,7 @@ export const AppDataSource = new DataSource({
     database: "test",
     synchronize: true,
     logging: false,
-    entities: [User],
+    entities: [Photo],
     migrations: [],
     subscribers: [],
-})
+});
