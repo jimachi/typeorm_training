208d5779ed6a4e1ca136a1cd1e94fae237ffaf24
Yuichiro Iijima <jimaichiro@gmail.com>
2023-02-03 00:08:28 +0900

    change:  MySQL -> postgreSQL  add docker-compose

--- MyProject/src/data-source.ts
+++ MyProject/src/data-source.ts
@@ -3,9 +3,9 @@ import { DataSource } from "typeorm"
 import { User } from "./entity/User"
 
 export const AppDataSource = new DataSource({
-    type: "mysql",
+    type: "postgres",
     host: "localhost",
-    port: 3306,
+    port: 5432,
     username: "test",
     password: "test",
     database: "test",
